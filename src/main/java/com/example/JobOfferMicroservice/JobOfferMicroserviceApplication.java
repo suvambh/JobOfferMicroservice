8517382e8b215b1ddf2ab02ec87b345444270abf
suvam <fmahuyac@gmail.com>
2026-06-10 18:53:29 +0200

    Initial Setup

---
+++ src/main/java/com/example/JobOfferMicroservice/JobOfferMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.JobOfferMicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobOfferMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobOfferMicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/JobOfferMicroservice/JobOfferMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.JobOfferMicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobOfferMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
